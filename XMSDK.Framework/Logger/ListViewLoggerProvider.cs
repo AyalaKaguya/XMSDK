581f446588318f3811ba0c0b94d88b691c1024f1
AyalaKaguya <ayalakaguya@outlook.com>
2025-09-09 10:04:11 +0800

    refactor: simplify ListViewLoggerProvider and ListViewLogger constructors, improve parameter handling

--- XMSDK.Framework/Logger/ListViewLoggerProvider.cs
+++ XMSDK.Framework/Logger/ListViewLoggerProvider.cs
@@ -5,20 +5,13 @@ using XMSDK.Framework.Forms;
 
 namespace XMSDK.Framework.Logger;
 
-public class ListViewLoggerProvider : ILoggerProvider
+public class ListViewLoggerProvider(LoggerList loggerList) : ILoggerProvider
 {
     private readonly ConcurrentDictionary<string, ListViewLogger> _loggers = new();
 
-    private readonly LoggerList _loggerList;
-
-    public ListViewLoggerProvider(LoggerList loggerList)
-    {
-        _loggerList = loggerList ?? throw new ArgumentNullException(nameof(loggerList));
-    }
-
     public ILogger CreateLogger(string categoryName)
     {
-        return _loggers.GetOrAdd(categoryName, name => new ListViewLogger(name, _loggerList));
+        return _loggers.GetOrAdd(categoryName, name => new ListViewLogger(name, loggerList));
     }
 
     public void Dispose()
@@ -27,23 +20,17 @@ public class ListViewLoggerProvider : ILoggerProvider
     }
 }
 
-public class ListViewLogger : ILogger
+public class ListViewLogger(string name, LoggerList loggerList) : ILogger
 {
-    private readonly string _name;
-    private readonly LoggerList _loggerList;
-
-    public ListViewLogger(string name, LoggerList loggerList)
-    {
-        _name = name;
-        _loggerList = loggerList;
-    }
-
-    public IDisposable BeginScope<TState>(TState state) => null!;
 
     public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return null;
+    }
 
-    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception,
-        Func<TState, Exception, string> formatter)
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
     {
         if (!IsEnabled(logLevel))
             return;
@@ -54,6 +41,6 @@ public class ListViewLogger : ILogger
             message += Environment.NewLine + exception;
         }
 
-        _loggerList.AddLogEntry(DateTime.Now, logLevel, message, _name);
+        loggerList.AddLogEntry(DateTime.Now, logLevel, message, name);
     }
 }
